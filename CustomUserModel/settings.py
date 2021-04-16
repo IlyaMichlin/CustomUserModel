2726365a67fc0ff999ea2cda4b5a923b35741ba5
Ilya <ilyamichlin1@gmail.com>
2021-04-16 16:35:06 +0300

    Added log in, log out, register.

--- CustomUserModel/settings.py
+++ CustomUserModel/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login redirect links
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
