8fa3f2329a0c8f10e8b173171bcb7d1cf4533bad
Ilya <ilyamichlin1@gmail.com>
2021-04-20 22:02:27 +0300

    Article management app

--- CustomUserModel/settings.py
+++ CustomUserModel/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'accounts',
     'pages',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jerusalem'
 
 USE_I18N = True
 
